886b2473085d9565c70e4b10d195fbdf249b8c6a
Akshay Urankar <101685739+evilieswithin@users.noreply.github.com>
2023-08-01 12:24:10 +0530

    react-router-dom setup along with changes in the script file as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/Signin/SignIn";
+import SignUp from "./pages/Signup/SignUp";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <>
-      <h1>Hello !!</h1>
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
