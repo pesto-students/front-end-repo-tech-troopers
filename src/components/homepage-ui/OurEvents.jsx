31de7cbfc57449e0149fd4f8bd2d6d74e45b75b8
Akshay Urankar <101685739+evilieswithin@users.noreply.github.com>
2023-08-11 15:59:28 +0530

    created the the fucntion which fetches the day and date

--- src/components/homepage-ui/OurEvents.jsx
+++ src/components/homepage-ui/OurEvents.jsx
@@ -4,19 +4,20 @@ import EventCard from './EventCard';
 
 const OurEvents = () => {
   return (
-    <section className="flex">
+    <section className="flex mt-10 mx-10">
       {/* Section Top */}
-      <div className="flex items-center justify-between mb-4">
-        <SectionHeading
-          heading="Our Events"
-          title="Join Upcoming Events Replays & Webinars"
-        />
-        <Button text="More Events" bgColor="#FF6D6D" textColor="#fff" />
+      <div className="flex flex-col items-center justify-between mb-4">
+        <div className="flex items-center">
+          <SectionHeading
+            heading="Our Events"
+            title="Join Upcoming Events Replays & Webinars"
+          />
+          <Button text="More Events" bgColor="#FF6D6D" textColor="#fff" />
+        </div>
         <EventCard />
       </div>
 
       {/* section bottom */}
-      {/* <EventCard /> */}
     </section>
   );
 };
