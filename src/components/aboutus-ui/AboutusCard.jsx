70df8212392ba4c0f33484be842d6d71bb47b487
Akshay Urankar <101685739+evilieswithin@users.noreply.github.com>
2023-08-16 16:04:37 +0530

    fixed about us page ui

--- src/components/aboutus-ui/AboutusCard.jsx
+++ src/components/aboutus-ui/AboutusCard.jsx
@@ -4,12 +4,12 @@ import SectionHeading from '../sectionHeading/SectionHeading';
 
 const AboutusCard = () => {
   return (
-    <section className="flex items-center space-x-6">
-      <div className="max-w-lg flex flex-col items-end">
-        <img src={Water} alt="" className="h-80 w-96" />
-        <div className="bg-dark text-white p-6">
-          <p className="flex items-center space-x-2 text-white text-xl font-shippori">
-            <img src={Quotes} className="h-10 w-10" alt="" />
+    <section className='flex items-center justify-center space-x-16'>
+      <div className='max-w-lg flex flex-col items-end'>
+        <img src={Water} alt='' className='h-80 w-96' />
+        <div className='bg-dark text-white p-6'>
+          <p className='flex items-center space-x-2 text-white text-xl font-shippori'>
+            <img src={Quotes} className='h-10 w-10' alt='' />
             <span>
               No act of kindness, no matter how small, is ever wasted.
             </span>
@@ -17,12 +17,12 @@ const AboutusCard = () => {
         </div>
       </div>
 
-      <div className="max-w-lg space-y-4">
+      <div className='max-w-lg space-y-4'>
         <SectionHeading
-          heading="About Us"
-          title="Solutions to Help People in Need and Save the Planet"
+          heading='About Us'
+          title='Solutions to Help People in Need and Save the Planet'
         />
-        <p className="text-gray-dark text-xl">
+        <p className='text-gray-dark text-lg'>
           At Brighter Days, we support, characterize and direct the effort and
           interests of various development activities, united by a common goal
           to remove poverty. We are deeply committed to influence governments
